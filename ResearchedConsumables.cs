49f0dd5bd4aef6369ace9392115c91c025a0e65c
Tyfyter <tyfyter2616@gmail.com>
2023-07-15 21:09:48 -0600

    1.4.4 release

--- ResearchedConsumables.cs
+++ ResearchedConsumables.cs
@@ -9,44 +9,44 @@ namespace ResearchedConsumables
 {
 	public class ResearchedConsumables : Mod {}
 	public class ResearchedGlobalItem : GlobalItem {
-		static Dictionary<int, int> neededSacrificeCounts => CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId;
+		static Dictionary<int, int> NeededSacrificeCounts => CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId;
 		public override bool AppliesToEntity(Item entity, bool lateInstantiation) {
 			return entity.consumable || entity.useAmmo > 0;
 		}
 		public override bool ConsumeItem(Item item, Player player) {
-			if (OriginConfig.Instance.ConsumeItem && player.whoAmI == Main.myPlayer && neededSacrificeCounts.TryGetValue(item.type, out int needed) && Main.LocalPlayerCreativeTracker.ItemSacrifices.GetSacrificeCount(item.type) >= needed) {
+			if (ResearchedConsumablesConfig.Instance.ConsumeItem && player.whoAmI == Main.myPlayer && ((item.createTile == -1 && item.createWall == -1) || ResearchedConsumablesConfig.Instance.Tiles) && HasResearched(item.type, player)) {
 				return false;
 			}
 			return true;
 		}
 		public override bool CanBeConsumedAsAmmo(Item ammo, Item weapon, Player player) {
-			if (OriginConfig.Instance.CanBeConsumedAsAmmo && player.whoAmI == Main.myPlayer && neededSacrificeCounts.TryGetValue(ammo.type, out int needed) && Main.LocalPlayerCreativeTracker.ItemSacrifices.GetSacrificeCount(ammo.type) >= needed) {
+			if (ResearchedConsumablesConfig.Instance.CanBeConsumedAsAmmo && player.whoAmI == Main.myPlayer && HasResearched(ammo.type, player)) {
 				return false;
 			}
 			return true;
 		}
 		public override bool NeedsAmmo(Item item, Player player) {
-			if (OriginConfig.Instance.NeedsAmmo && player.whoAmI == Main.myPlayer && neededSacrificeCounts.TryGetValue(item.useAmmo, out int needed) && Main.LocalPlayerCreativeTracker.ItemSacrifices.GetSacrificeCount(item.useAmmo) >= needed) {
+			if (ResearchedConsumablesConfig.Instance.NeedsAmmo && player.whoAmI == Main.myPlayer && HasResearched(item.useAmmo, player)) {
 				return false;
 			}
 			return true;
 		}
+		static bool HasResearched(int type, Player player) => NeededSacrificeCounts.TryGetValue(type, out int needed) && player.creativeTracker.ItemSacrifices.GetSacrificeCount(type) >= needed;
 	}
-	[Label("Settings")]
-	public class OriginConfig : ModConfig {
-		public static OriginConfig Instance;
+	public class ResearchedConsumablesConfig : ModConfig {
+		public static ResearchedConsumablesConfig Instance;
 		public override ConfigScope Mode => ConfigScope.ServerSide;
 
-		[Label("Affect ConsumeItem")]
 		[DefaultValue(true)]
 		public bool ConsumeItem = true;
 
-		[Label("Affect CanBeConsumedAsAmmo")]
 		[DefaultValue(true)]
 		public bool CanBeConsumedAsAmmo = true;
 
-		[Label("Affect NeedsAmmo")]
 		[DefaultValue(true)]
 		public bool NeedsAmmo = true;
+
+		[DefaultValue(true)]
+		public bool Tiles = true;
 	}
 }
